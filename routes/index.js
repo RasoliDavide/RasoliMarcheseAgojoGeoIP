c62a331b9107367de5ea68ed5ffa0242ef0a4eb2
Davide Rasoli <rasoli.davide@iisgalvanimi.edu.it>
2020-01-15 10:46:07 +0000

    drg

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var requestIp = require('request-ip')
+var requestIp = require('request-ip');
+var httpReqSender = require('axios');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    let ip = requestIp.getClientIp(req);
-    res.render('index', { title: 'Express', ip : ip });
+    if(req.body.ip == null)
+    {
+        let ip = requestIp.getClientIp(req);
+        let dati = null;
+
+        
+        console.log(dati)
+        res.render('index', { title: 'Express', ip : ip });
+    }
+    
 });
 router.post('/getIP', function(req, res, next)
 {
     console.log(req.body);
+    let ip = req.body.ip;
+    var dati = null;
+    httpReqSender.get(`http://ip-api.com/json/${ip}`)
+        .then(response => {
+        console.log(response.data.url);
+        console.log(response.data.body);
+    })
+    .catch(error => {
+        console.log(error);
+    });
     res.send(req.body);
 })
 module.exports = router;
